28962469a89bcb99ffed5cf8fa12dc7e05a5adf8
nyamsurenm <54369810+nyamsurenm@users.noreply.github.com>
2019-08-21 12:29:11 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*Hello
+Lab 1
+Mergen Nyamsuren	
+8/21/2019
+CMSC 255 - 004
+*/
+public class Hello {
+	public static void printHeading() {
+	System.out.println("Mergen");
+	System.out.println("Hello");
+	System.out.println("Lab 1");
+	System.out.println("8/21/2019");
+	System.out.println("CMSC 255 - 004");
+	System.out.println();
+	}
+	
+	public static void main(String[] args) {
+	printHeading();
+	System.out.println("Hello");
+	}
+}
+	
